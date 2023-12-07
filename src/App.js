978f03f2d2813d9d5fac5994e4a038d41c113a8f
Cihan <cihnyilmz@gmail.com>
2023-12-07 22:52:15 +0100

    initialized project and get acces token from Spotify API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+    const clientId = "8e4f4998d37544d896788cd73160b147";
+    const clientSecret = "d6f5d1f2b50e46adba03b7aa756243c5";
+
+    fetch(
+      "https://accounts.spotify.com/api/token", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: `grant_type=client_credentials&client_id=${clientId}&client_secret=${clientSecret}`,
+    })
+      .then(response => response.json())
+      .then(data => {
+        // Handle the response data
+        console.log(data);
+        setToken(data.access_token);
+        console.log(token);
+      })
+      .catch(error => {
+        // Handle errors
+        console.error('Error:', error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <h1>Spotify React</h1>
+        <h2>Token Data: {token}</h2>
+      </div>
     </div>
   );
 }
